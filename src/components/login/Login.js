7850b9ea37a6de947b3c72dcc730f7b3b185943c
Shaibal <shaibalsharif@gmail.com>
2022-03-02 05:32:12 +0600

    login work needed

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,30 +1,50 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useNavigate ,} from 'react-router-dom';
 
 const Login = () => {
-    const login=(e)=>{
+    const navigate = useNavigate()
+    const login = (e) => {
         //console.log(inputUser);
         e.preventDefault()
-      
+        localStorage.setItem('login', JSON.stringify(user))
+        const x= document.getElementById('error')
+        if(user.pass=='pass'){
+            x.innerHTML=""
+          
+           navigate('/') 
+        }
+        else{
+     
+     x.innerHTML="WRONG ID & PASS"
+        }
+        
     }
     //const navigate= useNavigate()
-    let inputUser={userName:"", pass:""}
+
+    const [user, setUser] = useState({ userName: "", pass: "" })
+    // const [pass,setPass]=useState("")
+
     const handleChange = (e) => {
-            inputUser[e.target.name] = e.target.value
-            localStorage.setItem('login', JSON.stringify(inputUser))
-                //navigate('/')
+        setUser({
+        ...(user),
+    [e.target.name] : e.target.value
+        })
+            
     };
 
-    return (
-     <div className=' page-wrapper'>
-        <div className="container "style={{height:'100vh'}}>
-            <div className="card  " style={{ marginTop:"15%" ,marginLeft:"auto",marginRight:'auto', width: '320px' }}>
-                <div className="card-body"style={{}}>
+return (
+    <div className=' page-wrapper'>
+        <div className="container " style={{ height: '100vh' }}>
+            <div className='text-center'>
+                <p id="error"></p>
+            </div>
+            <div className="card  " style={{ marginTop: "15%", marginLeft: "auto", marginRight: 'auto', width: '320px' }}>
+                <div className="card-body" style={{}}>
                     <h4 className="card-heading">Login Form</h4>
                     <form onSubmit={login}>
                         <div className="form-group">
                             <label htmlFor="userName">User Name</label>
-                            <input type="text" className="form-control" id="userName" name="userName" placeholder="Enter user name" onChange={handleChange}/>
+                            <input type="text" className="form-control" id="userName" name="userName" placeholder="Enter user name" onChange={handleChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="pass">Password</label>
@@ -35,8 +55,8 @@ const Login = () => {
                 </div>
             </div>
         </div>
-        </div>
-    )
+    </div>
+)
 }
 
 export default Login
